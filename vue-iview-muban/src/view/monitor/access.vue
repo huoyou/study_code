3bfb3017b6c93179d3541548a41569ed0adffc8d
xujie <jie.xu@ufscs.com>
2019-05-08 18:38:15 +0800

    fix

--- vue-iview-muban/src/view/monitor/access.vue
+++ vue-iview-muban/src/view/monitor/access.vue
@@ -2,14 +2,17 @@
     <div style="height: 100%">
         <Card style="height: 100%">
         <upload></upload>
+        <upload-img></upload-img>
         </Card>
     </div>
 </template>
 <script>
 import upload from '_c/common/upload'
+import uploadImg from '_c/common/uploadImg'
 export default {
     components: {
-        upload
+        upload,
+        uploadImg
     }
 }
 </script>
