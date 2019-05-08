2ab080c0f3e68d9fae8e6b1a3296ca2de88d9a6c
xujie <jie.xu@ufscs.com>
2019-05-08 18:07:08 +0800

    修改iview模板

--- vue-iview-muban/src/view/monitor/access.vue
+++ vue-iview-muban/src/view/monitor/access.vue
@@ -1,3 +1,15 @@
 <template>
-    <div>hahahaha</div>
+    <div style="height: 100%">
+        <Card style="height: 100%">
+        <upload></upload>
+        </Card>
+    </div>
 </template>
+<script>
+import upload from '_c/common/upload'
+export default {
+    components: {
+        upload
+    }
+}
+</script>
